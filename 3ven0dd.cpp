378f55cf5c835edcd01192628d029bb4f08df06e
raagh <rk00968@surrey.ac.uk>
2024-06-24 01:41:45 +0530

    even odd test

---
+++ 3ven0dd.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Check if input is even or odd" << endl;
+    int a;
+    cin >> a;
+
+    int b = 0;
+    int c;
+
+    return 0;
+}
+
