51f0ad55f4d96b73f14b157827e7f818a071d76e
raagh <rk00968@surrey.ac.uk>
2024-06-24 17:44:41 +0530

    Pointers and array stored in heap (C++ method)

--- p0inters.cpp
+++ p0inters.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main () {
 
     int *p;
-    p = (int *) malloc(5*sizeof(int));  //array of size 5 created in heap
+    p = new int [5];  //array of size 5 created in heap (C++ code)
     p[0] = 10; p[1] = 15; p[2] = 20; p[3] = 21; p[4] = 31;  //values initialized in array
 
     for (int i = 0; i < 5;i++)
