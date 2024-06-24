f03f538d17da51c07a0eb5831a494b781cefa006
raagh <rk00968@surrey.ac.uk>
2024-06-24 17:49:03 +0530

    How to deallocate heap memory (best practice) for C/C++

--- p0inters.cpp
+++ p0inters.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main () {
 
     int *p;
-    p = new int [5];  //array of size 5 created in heap (C++ code)
+    p = new int [5];  //array of size 5 created in heap (C++ code) (Dynamic allocation)
     p[0] = 10; p[1] = 15; p[2] = 20; p[3] = 21; p[4] = 31;  //values initialized in array
 
     for (int i = 0; i < 5;i++)
@@ -13,5 +13,7 @@ int main () {
     // p is a pointer which will act like the name of an array
     // and the array in this case is created inside the stack
 
+    delete [ ] p; //to release the dynamically allocated memory in c++
+    //for C use : free(p); to deallocate the dynamically allocated memory in heap
     return 0;
 }
