feeeff274a6d2c8d228f9f0886491fc6c7c848c2
raagh <rk00968@surrey.ac.uk>
2024-07-03 16:40:42 +0530

    Pointer to struct (rev2)

--- pointertostruct.cpp
+++ pointertostruct.cpp
@@ -3,16 +3,19 @@
 using namespace std;
 
 struct Rectangle{
-    int length;
+    int length;  //4 bytes for integer
     int breadth;
 };
 
 int main(){
-    struct Rectangle r = {10,5}; // in C++ writing only Rectangle r is also valid, however in C lang you need to say struct before that
-    cout << r.length << endl;
-    cout << r.breadth << endl;
 
-    Rectangle *p = &r;
+    Rectangle *p;
+    p = (struct Rectangle *)malloc(sizeof(struct Rectangle)); //this will allocate the memory of the structure in the heap memory
+    //typecasting
+
+    p -> length = 15;
+    p -> breadth = 7;
+
     cout << p -> length << endl;
     cout << p -> breadth << endl;
 
